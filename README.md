a97a520bddfe5c2fce0e25620493729693db7828
Evandro Franco <evandrogfranco@gmail.com>
2021-05-14 15:09:33 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Serverless PyTorch
+
+TBD
---
+++ app.py
@@ -0,0 +1,45 @@
+import urllib
+import json
+import os
+
+import torch
+import torch.nn as nn
+from PIL import Image
+from torchvision import transforms, models
+import numpy as np
+
+
+transform_test = transforms.Compose([
+    transforms.Resize(256),
+    transforms.CenterCrop(224),
+    transforms.ToTensor(),
+    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
+])
+
+model = models.resnet18(pretrained=False)
+num_ftrs = model.fc.in_features        
+model.fc = nn.Sequential(
+    nn.Linear(num_ftrs, 256),
+    nn.ReLU(),
+    nn.Dropout(0.4),
+    nn.Linear(256, 4), 
+    nn.LogSoftmax(dim=1) 
+)
+model.load_state_dict(torch.load('model/model.pth'))
+model.eval()
+
+objects_category = ['01-beer-mug','02-coffee-mug','03-teapot','04-wine-bottle']
+
+def lambda_handler(event, context):
+    url = event['queryStringParameters']['url']
+    
+    img = Image.open(urllib.request.urlopen(url))
+    scaled_img = transform_test(img)
+    torch_images = scaled_img.unsqueeze(0)
+
+    with torch.no_grad():
+            predict = model(torch_images)
+            output = torch.exp(predict)
+            index = np.argmax(output)
+
+    print("Result --> label: " + objects_category[index] + " | probability: " + str(output[0][index]))
